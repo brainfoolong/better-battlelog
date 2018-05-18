8069652bcee7436250ec5e8eb3eda10a4148bb2f
BrainFooLong <roland.eigelsreiter@gmail.com>
2018-05-18 12:20:27 +0200

    fixed redirect

--- scripts/p.js
+++ scripts/p.js
@@ -1,6 +1,10 @@
 'use strict';
 
 (function () {
+  if(location.pathname.substr(-1) === "/"){
+    location.pathname = location.pathname.substring(0, location.pathname.length - 1)
+    return
+  }
   var m = location.href.match(/board\/topic\/([0-9]+)/i)
   if (m) {
     location.href = '/forums/' + m[1] + '.html'
