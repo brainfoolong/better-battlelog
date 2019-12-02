2cc3e0be41cd398d75ff389ef1007d011d4cd696
Roland Eigelsreiter <brainfoolong@users.noreply.github.com>
2019-12-02 17:18:18 +0100

    Update t.js

--- scripts/t.js
+++ scripts/t.js
@@ -6,6 +6,10 @@ _paq.push(['trackPageView']);
 
 (function () {
   var u = 'https://matomo.0x.at/'
+  _paq.push(['disableCookies'])
+  _paq.push(['setCookieDomain', '*.nullix.at'])
+  _paq.push(['trackPageView'])
+  _paq.push(['enableLinkTracking']);
   _paq.push(['setTrackerUrl', u + 't.php'])
   _paq.push(['setSiteId', '3'])
   var d = document, g = d.createElement('script'), s = d.getElementsByTagName('script')[0]
